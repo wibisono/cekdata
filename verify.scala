59d6f9feb4e63ed840e702c395495e121ee69d3c
Adianto Wibisono <awibisono@ripe.net>
2020-04-19 19:15:48 +0200

    Add header

--- verify.scala
+++ verify.scala
@@ -1,11 +1,11 @@
 #!/usr/local/bin/amm
 import $ivy.`com.github.tototoshi::scala-csv:1.3.6`
 import $ivy.`com.lihaoyi::requests:0.1.4`
-import $ivy.`com.lihaoyi::ujson:1.0.0`
 import $ivy.`net.ruippeixotog::scala-scraper:2.2.0`
 
-import ujson._
 import scala.util._
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
@@ -19,12 +19,16 @@ val browser = JsoupBrowser()
 val okWriter  = CSVWriter.open("data/verified.csv")
 val errWriter  = CSVWriter.open("data/nodata.csv")
 
+okWriter.writeRow(List("kode_rs" , "nama_rs" , "total_tt" , "lk_terpakai" , "pr_terpakai" , "terpakai" , "lk_kosong" , "pr_kosong" , "kosong" , "wait_list" , "tgl_update"))
+errWriter.writeRow(List("kode_rs" , "nama_rs" , "info" ))
 
 faskes.foreach{ rs =>
    val kode = rs("kode_rs")
    val bed_monitor = s"http://sirs.yankes.kemkes.go.id/integrasi/data/bed_monitor.php?satker=$kode"
    try {
+     println(s"fetching bed monitor for ${rs("nama_rs")}")
      val rsdata = browser.get(bed_monitor) 
+     println(s"processing ${rs("nama_rs")}")
      val rows = rsdata >> element("tbody") >> elementList("tr") >> elementList("td") >> text("td")
      // Trying to get the last updated (excluding last row thus init)
      val updated = rows.init.map(_.last).toSeq.sorted.head
